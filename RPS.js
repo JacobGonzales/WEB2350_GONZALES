8c5be9b56c3cab5bc665e37a4c28f69e46951aea
Jacob Gonzales <35240561+JacobGonzales@users.noreply.github.com>
2018-04-24 19:07:45 -0600

    Add files via upload

---
+++ RPS.js
@@ -0,0 +1,80 @@
+$(document).ready(function(){
+    $("select").imagepicker({
+         hide_select:  false,
+        show_label:   true,
+    });
+
+    
+    $("select").data('picker');
+    
+    var choice = "";
+});
+        
+//    $("select").imagepicker();
+//    
+//    
+//    alert(choice);
+//        
+//    var cChoice = Math.random();
+//    if (cChoice < 0.34) {
+//        cChoice = "rock";
+//    } 
+//
+//    else if(cChoice <= 0.67) {
+//        cChoice = "paper";
+//    } 
+//
+//    else {
+//        cChoice = "scissors";
+//    }
+//
+//
+//        if (choice === "paper") {
+//
+//            if (cChoice === "rock") {
+//                alert("You Win!");
+//
+//            } 
+//            if else (cChoice === choice){
+//                alert("Tie!");
+//            }
+//            else {
+//                if (cChoice === "scissors") {
+//                    alert("Computer Wins!");
+//                }
+//            }
+//        }
+//        //
+//        if (choice === "rock") {
+//        
+//            if (cChoice === "scissors") {
+//                alert("You Win!");
+//
+//            } 
+//            if else (cChoice === choice){
+//                alert("Tie!");
+//            }
+//            else {
+//                if (cChoice === "paper") {
+//                    alert("Computer Wins!");
+//                }
+//            }
+//        }
+//        //
+//        if (choice === "scissors") {
+//        
+//            if (cChoice === "paper") {
+//                alert("You Win!");
+//
+//            } 
+//            if else (cChoice === choice){
+//                alert("Tie!");
+//            }
+//            else {
+//                if (cChoice === "rock") {
+//                    alert("Computer Wins!");
+//                }
+//            }
+//        }
+//    }
+//});
